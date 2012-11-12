c8af38bc29408086cfbf54cd9edd539b1c2cda36
Xiangfu <xiangfu@openmobilefree.net>
2012-11-12 12:26:56 +0800

    firmware: add all tqg144/ftg256/csg324 ucf files

---
+++ firmware/build_all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Use `git clean -xdf` for clean generate files. include the bits/ folder
+#  MAKE SURE you have commit all your new files and change
+#  MAKE SURE you run this command in CURRECT folder
+
+source /opt/Xilinx/14.2/ISE_DS/settings64.sh && \
+touch *.v && \
+FPGA_TARGET=xc6slx9-2-csg324 make && \
+touch *.v && \
+FPGA_TARGET=xc6slx9-2-ftg256 make && \
+touch *.v && \
+FPGA_TARGET=xc6slx9-2-tqg144 make
